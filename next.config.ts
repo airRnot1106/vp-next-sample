d67a17913f94bc6a421e0c802dcd7bf5d383440e
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-04-01 23:01:42 +0900

    chore: configure caching, logging, and typed routes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,25 @@
 import type { NextConfig } from 'next';
 
+// @keep-sorted
 const nextConfig: NextConfig = {
   /* config options here */
+  cacheComponents: true,
+  cacheLife: {
+    infinite: {
+      stale: Number.MAX_VALUE,
+      revalidate: Number.MAX_VALUE,
+      expire: Number.MAX_VALUE,
+    },
+  },
+  // @keep-sorted
+  logging: {
+    browserToTerminal: true,
+    fetches: {
+      fullUrl: true,
+    },
+  },
   reactCompiler: true,
+  typedRoutes: true,
 };
 
 export default nextConfig;
