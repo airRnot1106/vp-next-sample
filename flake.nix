19317a632062ebffa97edad2d9c2422e9d0b062e
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-04-05 20:54:52 +0900

    chore: add nix flake and direnv configuration

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  inputs = {
+    nix-vite-plus = {
+      url = "github:ryoppippi/nix-vite-plus";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nix-vite-plus,
+      nixpkgs,
+      systems,
+      treefmt-nix,
+      ...
+    }:
+    let
+      eachSystem =
+        f:
+        nixpkgs.lib.genAttrs (import systems) (
+          system:
+          f {
+            inherit system;
+            pkgs = nixpkgs.legacyPackages.${system};
+          }
+        );
+    in
+    {
+      devShells = eachSystem (
+        { pkgs, system }:
+        {
+          default = pkgs.mkShell {
+            packages = [
+              nix-vite-plus.packages.${system}.vp
+            ];
+          };
+        }
+      );
+      formatter = eachSystem (
+        { pkgs, ... }:
+        treefmt-nix.lib.mkWrapper pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixfmt.enable = true;
+          };
+        }
+      );
+    };
+}
