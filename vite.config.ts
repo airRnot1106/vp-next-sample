d8516342d0292dc2458cd8bc5ba9614a57d1ad46
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-04-24 15:12:37 +0900

    chore(config): configure code formatting rules

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,31 @@
-import { defineConfig } from "vite-plus";
+import { defineConfig } from 'vite-plus';
 
 export default defineConfig({
   staged: {
-    "*": "vp check --fix",
+    '*': 'vp check --fix',
   },
-  fmt: {},
+  fmt: {
+    semi: true,
+    singleQuote: true,
+    sortImports: {
+      groups: [
+        ['type-builtin', 'value-builtin'],
+        ['type-external', 'type-internal', 'value-external', 'value-internal'],
+        [
+          'type-index',
+          'type-parent',
+          'type-sibling',
+          'value-index',
+          'value-parent',
+          'value-sibling',
+        ],
+        ['unknown'],
+      ],
+      newlinesBetween: true,
+      order: 'asc',
+    },
+    sortPackageJson: true,
+    trailingComma: 'all',
+  },
   lint: { options: { typeAware: true, typeCheck: true } },
 });
