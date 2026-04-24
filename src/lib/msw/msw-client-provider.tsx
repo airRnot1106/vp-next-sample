fe280c9b24d14481ead84c1759b5cff800ec87f1
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-04-24 16:13:53 +0900

    chore: init msw

---
+++ src/lib/msw/msw-client-provider.tsx
@@ -0,0 +1,18 @@
+'use client';
+import { ReactNode, use } from 'react';
+
+// MSW ワーカーを初期化する非同期処理（Promise）
+const mockingEnabledPromise =
+  typeof window !== 'undefined'
+    ? // oxlint-disable-next-line promise/always-return
+      import('../../../mocks/browser').then(async ({ worker }) => {
+        await worker.start({ onUnhandledRequest: 'bypass' });
+      })
+    : Promise.resolve();
+
+// MSW クライアントプロバイダー
+export const MswClientProvider = ({ children }: { children: ReactNode }) => {
+  // useでPromise（MSW の初期化完了）を待機 -> Promise が解決後子要素をレンダリング
+  use(mockingEnabledPromise);
+  return children;
+};
