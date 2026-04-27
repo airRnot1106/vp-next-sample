9100599b452b9d8672c46caaeb8db70bb9399ee9
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-04-27 16:07:52 +0900

    style: format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,23 @@ export default function Home() {
   return (
     <div>
       <main>
-        <Image src="/next.svg" alt="Next.js logo" width={100} height={20} priority />
+        <Image priority alt="Next.js logo" height={20} src="/next.svg" width={100} />
         <div>
           <h1>To get started, edit the page.tsx file.</h1>
           <p>
             Looking for a starting point or more instructions? Head over to{' '}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
               rel="noopener noreferrer"
+              target="_blank"
             >
               Templates
             </a>{' '}
             or the{' '}
             <a
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
               rel="noopener noreferrer"
+              target="_blank"
             >
               Learning
             </a>{' '}
@@ -30,16 +30,16 @@ export default function Home() {
         <div>
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
             rel="noopener noreferrer"
+            target="_blank"
           >
-            <Image src="/vercel.svg" alt="Vercel logomark" width={16} height={16} />
+            <Image alt="Vercel logomark" height={16} src="/vercel.svg" width={16} />
             Deploy Now
           </a>
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
             rel="noopener noreferrer"
+            target="_blank"
           >
             Documentation
           </a>
